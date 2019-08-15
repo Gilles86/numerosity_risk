f67b03821fd9181d562614f683cd91f8ca6c9e0a
Gilles de Hollander <gilles.de.hollander@gmail.com>
2019-08-15 12:25:57 +0200

    Updated smooth data

--- preproc/smooth_data.py
+++ preproc/smooth_data.py
@@ -11,57 +11,56 @@ from niworkflows.interfaces.bids import DerivativesDataSink
 import argparse
 
 
-sourcedata = '/home/gholland/data/risk_precision/ds-numrisk'
-derivatives = op.join(sourcedata, 'derivatives')
-subject = 14
+def main(subject, sourcedata):
+    derivatives = op.join(sourcedata, 'derivatives')
 
-derivatives_fmriprep = op.join(derivatives, 'fmriprep')
-derivatives_freesurfer = op.join(derivatives, 'freesurfer')
-os.environ['SUBJECTS_DIR'] = derivatives_freesurfer
+    derivatives_fmriprep = op.join(derivatives, 'fmriprep')
+    derivatives_freesurfer = op.join(derivatives, 'freesurfer')
+    os.environ['SUBJECTS_DIR'] = derivatives_freesurfer
 
-fn_template = op.join(derivatives_fmriprep,
-    f'sub-{subject}',
-    'func',
-    f'sub-{subject}_task-numrisk_acq-*_run-*_space-fsaverage6_hemi-*.func.gii')
+    fn_template = op.join(derivatives_fmriprep,
+        f'sub-{subject}',
+        'func',
+        f'sub-{subject}_task-numrisk_acq-*_run-*_space-fsaverage6_hemi-*.func.gii')
 
-fns = glob.glob(fn_template)
+    fns = glob.glob(fn_template)
 
-workflow = pe.Workflow(name=f'smooth_sub-{subject}',
-        base_dir='/scratch')
+    workflow = pe.Workflow(name=f'smooth_sub-{subject}',
+            base_dir='/scratch')
 
-input_node = pe.Node(niu.IdentityInterface(fields=['surface_files']),
-        name='input_node')
-input_node.inputs.surface_files = fns
-
-
-def get_hemis(in_files):
-    import re
-
-    reg = re.compile('.*/(?P<subject>sub-[0-9]+)_task.*_hemi-(?P<hemi>L|R)\.func\.gii')
-    hemis = [reg.match(fn).group(2) for fn in in_files]
-    hemis = ['lh' if hemi == 'L' else 'rh' for hemi in hemis]
-
-    return hemis
+    input_node = pe.Node(niu.IdentityInterface(fields=['surface_files']),
+            name='input_node')
+    input_node.inputs.surface_files = fns
 
 
+    def get_hemis(in_files):
+        import re
 
-smoother = pe.MapNode(freesurfer.SurfaceSmooth(fwhm=5,
-    subject_id='fsaverage6'),
-    iterfield=['in_file', 'hemi'],
-        name='smoother')
+        reg = re.compile('.*/(?P<subject>sub-[0-9]+)_task.*_hemi-(?P<hemi>L|R)\.func\.gii')
+        hemis = [reg.match(fn).group(2) for fn in in_files]
+        hemis = ['lh' if hemi == 'L' else 'rh' for hemi in hemis]
 
-workflow.connect(input_node, 'surface_files', smoother, 'in_file')
-workflow.connect(input_node, ('surface_files', get_hemis), smoother, 'hemi')
+        return hemis
 
-ds = pe.MapNode(DerivativesDataSink(out_path_base='smoothed'),
-    iterfield=['source_file', 'in_file'], name='datasink')
-ds.inputs.base_directory = derivatives
-ds.inputs.desc = 'smoothed'
 
-workflow.connect(input_node, 'surface_files', ds, 'source_file')
-workflow.connect(smoother, 'out_file', ds, 'in_file')
 
-workflow.run(plugin='MultiProc', plugin_args={'n_procs':15})
+    smoother = pe.MapNode(freesurfer.SurfaceSmooth(fwhm=5,
+        subject_id='fsaverage6'),
+        iterfield=['in_file', 'hemi'],
+            name='smoother')
+
+    workflow.connect(input_node, 'surface_files', smoother, 'in_file')
+    workflow.connect(input_node, ('surface_files', get_hemis), smoother, 'hemi')
+
+    ds = pe.MapNode(DerivativesDataSink(out_path_base='smoothed'),
+        iterfield=['source_file', 'in_file'], name='datasink')
+    ds.inputs.base_directory = derivatives
+    ds.inputs.desc = 'smoothed'
+
+    workflow.connect(input_node, 'surface_files', ds, 'source_file')
+    workflow.connect(smoother, 'out_file', ds, 'in_file')
+
+    workflow.run(plugin='MultiProc', plugin_args={'n_procs':15})
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
@@ -71,5 +70,7 @@ if __name__ == '__main__':
 
     args = argparser.parse_args()
     print(args)
+
+    main(args.subject, args.data)
 
 
