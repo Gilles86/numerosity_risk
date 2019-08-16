36b5d3723bf396b72a2f12cec4127de49162ea3b
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2019-08-16 15:16:53 +0200

    new dockerfile that installs braincoder

--- make_dockerfile.sh
+++ make_dockerfile.sh
@@ -5,15 +5,18 @@ neurodocker generate docker --base ubuntu --pkg-manager apt --freesurfer version
        	--output=$DIR/Dockerfile \
   --install zsh wget git build-essential \
     --miniconda \
-      conda_install="python=3.7 pandas matplotlib scikit-learn seaborn ipython" \
+      conda_install="python=3.7 pandas matplotlib scikit-learn seaborn ipython tensorflow" \
       pip_install="nilearn
 		nipype
                   pybids
 		  nistats
-		  niworkflows" \
+		  niworkflows
+		  tensorflow_probability" \
       create_env="neuro" \
       activate=true \
    --run 'wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true' \
    --run 'conda init zsh' \
-   --run 'echo "conda activate neuro" >> ~/.zshrc' \
-   --workdir /src
+   --run 'echo "conda activate neuro" >> ~/.zshrc && conda init' \
+   --workdir /src \
+   --copy braincoder /braincoder \
+   --run-bash "source activate neuro && cd /braincoder && python setup.py develop --no-deps" \
