f1efc1e1cef2596a747d82b316d4213a366e6b89
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2019-10-04 11:14:24 +0200

    Make jobs for left and right hemisphere

--- fit_model/decode.py
+++ fit_model/decode.py
@@ -11,7 +11,9 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 
-def main(subject, sourcedata, trialwise, progressbar):
+def main(subject, sourcedata, trialwise,masks,  progressbar):
+
+    print(masks)
 
     derivatives = op.join(sourcedata, 'derivatives')
 
@@ -21,7 +23,8 @@ def main(subject, sourcedata, trialwise, progressbar):
                                     validate=False)
 
         bidsfiles = layout.get(subject=subject, extension='pe.gii')
-        bidsfiles = [e for e in bidsfiles if 'space-fsaverage6_desc-stims1_hemi-R' in e.path]
+        bidsfiles_l = [e for e in bidsfiles if 'space-fsaverage6_desc-stims1_hemi-L' in e.path]
+        bidsfiles_r = [e for e in bidsfiles if 'space-fsaverage6_desc-stims1_hemi-R' in e.path]
         stimuli = np.repeat([5, 7, 10, 14, 20, 28], 6)
 
     else:
@@ -30,33 +33,50 @@ def main(subject, sourcedata, trialwise, progressbar):
                                     validate=False)
 
         bidsfiles = layout.get(subject=subject, extension='pe.gii')
-        bidsfiles = [e for e in bidsfiles if 'space-fsaverage6_desc-stims1_hemi-R' in e.path]
+        bidsfiles_l = [e for e in bidsfiles if 'space-fsaverage6_desc-stims1_hemi-L' in e.path]
+        bidsfiles_r = [e for e in bidsfiles if 'space-fsaverage6_desc-stims1_hemi-R' in e.path]
 
         stimuli = [5, 7, 10, 14, 20, 28]
 
-    fns = [e.path for e in bidsfiles]
+    fns_l = [e.path for e in bidsfiles_l]
+    fns_r = [e.path for e in bidsfiles_r]
 
-    data = pd.concat([pd.DataFrame(surface.load_surf_data(fn).T,
-                     index=pd.Index(stimuli, name='stimulus'),) for fn in fns], 
+    data_l = pd.concat([pd.DataFrame(surface.load_surf_data(fn).T,
+                     index=pd.Index(stimuli, name='stimulus'),) for fn in fns_l], 
                       axis=0, 
-                      keys=[(subject, b.run) for b in bidsfiles], 
+                      keys=[(subject, b.run) for b in bidsfiles_l], 
                       names=['subject', 'run'])
+
+    data_r = pd.concat([pd.DataFrame(surface.load_surf_data(fn).T,
+                     index=pd.Index(stimuli, name='stimulus'),) for fn in fns_r], 
+                      axis=0, 
+                      keys=[(subject, b.run) for b in bidsfiles_r], 
+                      names=['subject', 'run'])
+
+    data = pd.concat((data_l, data_r), keys=['lh', 'rh'], names=['hemi'], axis=1)
+    print(data)
 
     data['log(stimulus)'] = np.log(data.index.get_level_values('stimulus'))
     data = data.set_index('log(stimulus)', append=True)
 
     stimulus_range = np.log(np.geomspace(1, 50, 200))
 
-    for mask_str in ['NPC1', 'NPC2', 'NPC3', 'NF1', 'NF2', 'NTO']:
-    #for mask_str in ['NF2']:
-        mask = surface.load_surf_data(op.join(derivatives, f'surface_masks/desc-{mask_str}_R_space-fsaverage6_hemi-rh.label.gii'))
+    for mask_str in masks:
+        
+        if mask_str[-1] == 'L':
+            hemi = 'lh'
+        elif mask_str[-1] == 'R':
+            hemi = 'rh'
+        else:
+            raise Exception('Which hemi is this mask?')
 
-        problem_mask = data.isnull().any(0) | (data.std(0) == 0) | (data.std(0) > data.std(0).mean() * 4)
-        print('ATTENTION')
-        print(data.std(0), data.std(0).mean(), (data.std(0) > data.std(0).mean() * 4).sum())
+        mask = surface.load_surf_data(op.join(derivatives, f'surface_masks.v2/desc-{mask_str}_space-fsaverage6_hemi-{hemi}.label.gii'))
+
+        d = data.loc[:, hemi]
+        problem_mask = d.isnull().any(0) | (d.std(0) == 0)
         mask = (mask > 0) & ~problem_mask.values
 
-        dist_matrix = op.join(derivatives, 'surface_masks', f'desc-distance_matrix-{mask_str}-R_space-fsaverage6_hemi-rh.gii')
+        dist_matrix = op.join(derivatives, 'surface_masks.v2', f'desc-distance_matrix-{mask_str}_space-fsaverage6_hemi-{hemi}.gii')
         dist_matrix = surface.load_surf_data(dist_matrix)
 
         skip_n = 1
@@ -72,64 +92,66 @@ def main(subject, sourcedata, trialwise, progressbar):
         parameters = []
         pdfs = []
 
+        mask_failed = False
         for run in range(1, 7):
-            logging.info(f'Working on MASK {mask_str} RUN {run}')
-            test = d.loc[(subject, run)].copy()
-            train = d.drop(run, level='run').copy()
+            if not mask_failed:
+                logging.info(f'Working on MASK {mask_str} RUN {run}')
+                test = d.loc[(subject, run)].copy()
+                train = d.drop(run, level='run').copy()
+                
+                model.fit_parameters(train.index.get_level_values('log(stimulus)'), train, progressbar=progressbar)
+
+                sm = model.to_stickmodel(basis_stimuli=stimulus_range)
+                
+                try:
+                    sm.fit_residuals(data=train, lambd=1.0, distance_matrix=distance_matrix,
+                                     progressbar=progressbar, residual_dist='gaussian')
+                except Exception as e:
+                    mask_failed = True
+                    break
+
+                lambd = 1.0
+
+                pdf, map, sd, (lower, upper) = sm.get_stimulus_posterior(test, stimulus_range=stimulus_range, normalize=True)
+
+                tmp = pd.concat((np.exp(map), map, sd, lower, upper),
+                                keys=['map', 'log(map)', 'sd', 'lower', 'upper'], axis=1)
+                tmp = tmp.droplevel(-1, 1)
+                tmp.index = test.index
+
+                #pdf = pd.DataFrame(pdf, index=test.index,)
+                print(pdf)
+
+                pdf = pd.concat((pdf, tmp), keys=['pdf', 'pars'], axis=1)
+                pdf = pd.concat([pdf], keys=[mask_str], names=['mask'])
+
+                par = pd.DataFrame({'rho':sm.rho, 'alpha':sm.alpha, 'beta':sm.beta, 'sigma2':sm.sigma2, 'lambda':lambd},
+                                    index=pd.MultiIndex.from_tuples([(subject, run, mask_str)], names=['subject', 'run', 'mask']))
+
+                parameters.append(par)
+                pdfs.append(pdf)
+
+        if not mask_failed:
+            parameters = pd.concat(parameters)
+            pdfs = pd.concat(pdfs)
             
-            model.fit_parameters(train.index.get_level_values('log(stimulus)'), train, progressbar=progressbar)
+            if trialwise:
+                base_dir = op.join(derivatives, 'decoding_trialwise', f'sub-{subject}', 'func')
+            else:
+                base_dir = op.join(derivatives, 'decoding_runwise', f'sub-{subject}', 'func')
 
-            sm = model.to_stickmodel(basis_stimuli=stimulus_range)
-            
-            sm.fit_residuals(data=train, lambd=1.0, distance_matrix=distance_matrix,
-                             progressbar=progressbar, residual_dist='gaussian')
-            lambd = 1.0
+            if not op.exists(base_dir):
+                os.makedirs(base_dir)
 
-            #print('DOF: {}'.format(sm.dof))
-
-            pdf, map, sd, (lower, upper) = sm.get_stimulus_posterior(test, stimulus_range=stimulus_range, normalize=True)
-
-            tmp = pd.concat((map, np.log(map), sd, lower, upper),
-                            keys=['map', 'log(map)', 'sd', 'lower', 'upper'], axis=1)
-            print(tmp)
-            tmp = tmp.droplevel(-1, 1)
-            print(tmp)
-            tmp.index = test.index
-
-            #pdf = pd.DataFrame(pdf, index=test.index,)
-            print(pdf)
-
-            pdf = pd.concat((pdf, tmp), keys=['pdf', 'pars'], axis=1)
-            pdf = pd.concat([pdf], keys=[mask_str], names=['mask'])
-
-            #par = pd.DataFrame({'rho':sm.rho, 'alpha':sm.alpha, 'beta':sm.beta, 'sigma2':sm.sigma2, 'lambda':lambd, 'dof':sm.dof},
-                                #index=pd.MultiIndex.from_tuples([(subject, run, mask_str)], names=['subject', 'run', 'mask']))
-            
-            par = pd.DataFrame({'rho':sm.rho, 'alpha':sm.alpha, 'beta':sm.beta, 'sigma2':sm.sigma2, 'lambda':lambd},
-                                index=pd.MultiIndex.from_tuples([(subject, run, mask_str)], names=['subject', 'run', 'mask']))
-
-            parameters.append(par)
-            pdfs.append(pdf)
-
-        parameters = pd.concat(parameters)
-        pdfs = pd.concat(pdfs)
-        
-        if trialwise:
-            base_dir = op.join(derivatives, 'decoding_trialwise', f'sub-{subject}', 'func')
-        else:
-            base_dir = op.join(derivatives, 'decoding_runwise', f'sub-{subject}', 'func')
-
-        if not op.exists(base_dir):
-            os.makedirs(base_dir)
-
-        parameters.to_pickle(op.join(base_dir, f'sub-{subject}_mask-{mask_str}_desc-decodepars.pkl'))
-        pdfs.to_pickle(op.join(base_dir, f'sub-{subject}_mask-{mask_str}_desc-decodepdfs.pkl'))
+            parameters.to_pickle(op.join(base_dir, f'sub-{subject}_mask-{mask_str}_desc-decodepars.pkl'))
+            pdfs.to_pickle(op.join(base_dir, f'sub-{subject}_mask-{mask_str}_desc-decodepdfs.pkl'))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--trialwise", action='store_true')
     parser.add_argument("--progressbar", action='store_true')
+    parser.add_argument("--masks", required=True, nargs='+')
     parser.add_argument("--sourcedata",
                         default='/data/risk_precision/ds-numrisk')
     parser.add_argument("subject", type=int)
@@ -139,4 +161,5 @@ if __name__ == '__main__':
     main(int(args.subject),
          sourcedata=args.sourcedata,
          trialwise=args.trialwise,
+         masks=args.masks,
          progressbar=args.progressbar)
