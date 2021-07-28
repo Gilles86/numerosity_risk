980c999605411167d84768a06eee9b34732b1014
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2021-07-28 09:07:31 +0000

    Updated out-of-sample-prediction-scripts

--- analysis/outofset/get_out_of_set_accuracies.py
+++ analysis/outofset/get_out_of_set_accuracies.py
@@ -2,13 +2,14 @@ import os
 import numpy as np
 import pandas as pd
 import os.path as op
+from itertools import product
+from utils import get_cv2_accuracy, get_null
 
-from utils import get_cv2_accuracy, get_accuracy_and_null
+bids_folder = '/data/ds-numrisk'
+overwrite = False
 
-bids_folder = '/data2/risk_precision/ds-numrisk'
-overwrite = True
-
-data = pd.read_csv(op.join(bids_folder, 'derivatives', 'summary_data','subjectwise_data.csv'))
+data = pd.read_csv(op.join(bids_folder, 'derivatives',
+                           'summary_data', 'subjectwise_data.csv'))
 
 data['subject'] = data['subject'].astype(np.int64)
 data = data.set_index('subject')
@@ -19,17 +20,31 @@ if not op.exists(target_dir):
     os.makedirs(target_dir)
 
 
-x = ['npc1_left_sd','npc1_right_sd']
-y = 'mag_noise3'
+xs = [['mag_noise3'], ['npc1_left_sd'], ['npc1_right_sd'], [
+    'npc1_left_sd', 'npc1_right_sd'], ['mag_noise3', 'npc1_left_sd', 'npc1_right_sd'],
+    ['npc1_left_r'],['npc1_right_r'], ['npc1_left_r', 'npc1_right_r'],
+    ['mag_noise3', 'npc1_left_r', 'npc1_right_r'], ]
 
-target_file_accuracy = op.join(target_dir, f'x-{".".join(x)}_y-mag_noise_accuracy.txt')
-target_file_null = op.join(target_dir, f'x-{".".join(x)}_y-mag_noise_null.txt')
+ys = ['mag_noise3', 'risknoise_coin', 'risknoise_sym', 'risknoise_pool', 
+        'rnp_sym', 'rnp_coin', 'rnp_pool']
 
-if (not op.exists(target_file_accuracy)) or overwrite:
-    accuracy = get_cv2_accuracy(data, x, y) 
 
-    np.savetxt(target_file_accuracy, [accuracy])
+for x, y in product(xs, ys):
 
-    accuracy, null = get_accuracy_and_null(data, x, y, n=100, n_jobs=7)
+    try: 
+        target_file_accuracy = op.join(
+            target_dir, f'x-{".".join(x)}_y-{y}_accuracy.txt')
+        target_file_null = op.join(target_dir, f'x-{".".join(x)}_y-{y}_null.txt')
 
-    np.savetxt(target_file_null, null)
+        if (not op.exists(target_file_accuracy)) or overwrite:
+
+            accuracy = get_cv2_accuracy(data, x, y)
+            print(f"Accuracy for model {y} ~ {' + '.join(x)}: {accuracy*100:.02f}%")
+
+            np.savetxt(target_file_accuracy, [accuracy])
+
+            null = get_null(data, x, y, n=1000, n_jobs=14)
+
+            np.savetxt(target_file_null, null)
+    except Exception as e:
+        print("Issue with {x} - {y}: {e}")
--- analysis/outofset/utils.py
+++ analysis/outofset/utils.py
@@ -1,3 +1,5 @@
+import contextlib
+import joblib
 from joblib import Parallel, delayed
 import numpy as np
 import pandas as pd
@@ -5,50 +7,69 @@ from sklearn.model_selection import LeaveOneOut, KFold, LeavePOut
 from sklearn import linear_model
 from tqdm import tqdm
 
-def get_out_of_set_accuracy(test, train, x, y):
-    
-    assert(len(test) == 2), 'Test set should contain 2 samples'
-    
-    model = linear_model.LinearRegression(fit_intercept=True)    
-    model.fit(train[x], train[y])
-    
-    predicted = pd.Series(model.predict(test[x]), index=test.index)
-    
-    return predicted.idxmax() == test[y].idxmax()
 
+@contextlib.contextmanager
+def tqdm_joblib(tqdm_object):
+    """Context manager to patch joblib to report into tqdm progress bar given as argument"""
+    class TqdmBatchCompletionCallback(joblib.parallel.BatchCompletionCallBack):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+        def __call__(self, *args, **kwargs):
+            tqdm_object.update(n=self.batch_size)
+            return super().__call__(*args, **kwargs)
+
+    old_batch_callback = joblib.parallel.BatchCompletionCallBack
+    joblib.parallel.BatchCompletionCallBack = TqdmBatchCompletionCallback
+    try:
+        yield tqdm_object
+    finally:
+        joblib.parallel.BatchCompletionCallBack = old_batch_callback
+        tqdm_object.close()
+
+
+def get_out_of_set_accuracy(test, train, x, y):
+
+    intercept_train = np.ones((len(train) ,1 ))
+    x_train = np.concatenate((intercept_train, train[x]), 1)
+
+    beta = np.linalg.lstsq(x_train, train[y], rcond=-1)[0]
+
+    intercept_test = np.ones((len(test), 1))
+    x_test = np.concatenate((intercept_test, test[x]), 1)
+    predicted = x_test @ beta
+
+    return np.argmax(predicted) == np.argmax(test[y])
 
 def get_cv2_accuracy(data, x, y, pbar=True):
-
 
     splitter = LeavePOut(2)
     n_splits = splitter.get_n_splits(data)
     correct = np.zeros(n_splits)
 
     splits = enumerate(splitter.split(data))
-    
+
     if pbar:
         splits = tqdm(splits, total=n_splits)
-        
+
     for ix, (train, test) in splits:
-        correct[ix] = get_out_of_set_accuracy(data.iloc[test], data.iloc[train], x, y)
-        
+        correct[ix] = get_out_of_set_accuracy(
+            data.iloc[test], data.iloc[train], x, y)
+
     return np.mean(correct)
 
 
-def get_accuracy_and_null(data, x, y, n=250, n_jobs=7):
-    
-    if type(x) is str:
-        x = [x]
-    
-    empirical_accuracy = get_cv2_accuracy(data, x, y)
-    
+def get_null(data, x, y, n=250, n_jobs=7):
+
     null_accuracies = np.zeros(n)
-    
+
     def shuffle_and_get_cv2_accuracy():
         shuffled_data = data.copy()
         shuffled_data[y] = shuffled_data[y].sample(frac=1.).values
         return get_cv2_accuracy(shuffled_data, x, y, pbar=False)
 
-    null_accuracies = Parallel(n_jobs=n_jobs)(delayed(shuffle_and_get_cv2_accuracy)() for i in range(n))
-    
-    return empirical_accuracy, null_accuracies
+    with tqdm_joblib(tqdm(desc="Null distro", total=n)) as progress_bar:
+        null_accuracies = Parallel(n_jobs=n_jobs)(
+            delayed(shuffle_and_get_cv2_accuracy)() for i in range(n))
+
+    return null_accuracies
