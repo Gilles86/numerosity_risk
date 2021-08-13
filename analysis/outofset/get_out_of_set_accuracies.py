c9ded95b4051e41ad720cf3444a4f86ccc210ef1
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2021-08-13 11:23:04 +0200

    adapted script ot new data

--- analysis/outofset/get_out_of_set_accuracies.py
+++ analysis/outofset/get_out_of_set_accuracies.py
@@ -5,46 +5,62 @@ import os.path as op
 from itertools import product
 from utils import get_cv2_accuracy, get_null
 
-bids_folder = '/data/ds-numrisk'
+bids_folder = '/data2/risk_precision/ds-numrisk'
 overwrite = False
 
-data = pd.read_csv(op.join(bids_folder, 'derivatives',
-                           'summary_data', 'subjectwise_data.csv'))
+neural_results = pd.read_csv(op.join(bids_folder, 'derivatives',
+                           'summary_data', 'neural_results_aug2021.tsv'),
+                           sep='\t', )
 
-data['subject'] = data['subject'].astype(np.int64)
-data = data.set_index('subject')
+neural_results = neural_results.set_index(['type', 'subject', 'mask']).loc['trialwise']
+
+behav = pd.read_csv(op.join(bids_folder, 'derivatives', 'summary_data', 'subjectwise_data.csv')).set_index('subject')
+behav.index = behav.index.astype(int)
+
+data = neural_results.join(behav)
+
+
 
 target_dir = op.join(bids_folder, 'derivatives', 'outofsetpredictions')
 
 if not op.exists(target_dir):
     os.makedirs(target_dir)
 
+xs = [['neural precision'], ['slope'], ['neural precision', 'slope']]
 
-xs = [['mag_noise3'], ['npc1_left_sd'], ['npc1_right_sd'], [
-    'npc1_left_sd', 'npc1_right_sd'], ['mag_noise3', 'npc1_left_sd', 'npc1_right_sd'],
-    ['npc1_left_r'],['npc1_right_r'], ['npc1_left_r', 'npc1_right_r'],
-    ['mag_noise3', 'npc1_left_r', 'npc1_right_r'], ]
+# xs = [['mag_noise3'], ['npc1_left_sd'], ['npc1_right_sd'], [
+    # 'npc1_left_sd', 'npc1_right_sd'], ['mag_noise3', 'npc1_left_sd', 'npc1_right_sd'],
+    # ['npc1_left_r'],['npc1_right_r'], ['npc1_left_r', 'npc1_right_r'],
+    # ['mag_noise3', 'npc1_left_r', 'npc1_right_r'], [left_minus_right_sd, ]
 
-ys = ['mag_noise3', 'risknoise_coin', 'risknoise_sym', 'risknoise_pool', 
-        'rnp_sym', 'rnp_coin', 'rnp_pool']
+ys = ['mag_prec']
+
+masks = ['NPC_R']
 
 
-for x, y in product(xs, ys):
+for mask in masks:
+    for x, y in product(xs, ys):
 
-    try: 
-        target_file_accuracy = op.join(
-            target_dir, f'x-{".".join(x)}_y-{y}_accuracy.txt')
-        target_file_null = op.join(target_dir, f'x-{".".join(x)}_y-{y}_null.txt')
+        try: 
+            target_file_accuracy = op.join(
+                target_dir, f'x-{".".join(x)}_y-{y}_accuracy.txt')
+            target_file_null = op.join(target_dir, f'x-{".".join(x)}_y-{y}_null.txt')
 
-        if (not op.exists(target_file_accuracy)) or overwrite:
+            if (not op.exists(target_file_accuracy)) or overwrite:
 
-            accuracy = get_cv2_accuracy(data, x, y)
-            print(f"Accuracy for model {y} ~ {' + '.join(x)}: {accuracy*100:.02f}%")
+                d = data.xs(mask, 0, 'mask')
 
-            np.savetxt(target_file_accuracy, [accuracy])
+                print(f'size d: {d.shape}')
 
-            null = get_null(data, x, y, n=1000, n_jobs=14)
+                accuracy = get_cv2_accuracy(d, x, y)
+                print(f"Accuracy for model {y} ~ {' + '.join(x)}: {accuracy*100:.02f}%")
 
-            np.savetxt(target_file_null, null)
-    except Exception as e:
-        print("Issue with {x} - {y}: {e}")
+                np.savetxt(target_file_accuracy, [accuracy])
+
+                null = get_null(d, x, y, n=100, n_jobs=7)
+
+                print(f'p-value: {(null>accuracy).mean()}")
+
+                np.savetxt(target_file_null, null)
+        except Exception as e:
+            print(f"Issue with {x} - {y}: {e}")
